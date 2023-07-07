019c4c3451ab5f7a418a035ef0980ccdd6aa1cc4
Dendzsi <137180234+Dendzsi@users.noreply.github.com>
2023-07-07 09:58:28 +0200

    Create Restaurant.java

---
+++ Restaurant.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Restaurant {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    Tables myTable = new Tables();    
+    myTable.choose();
+    
+    //Drink.Cola myDrink = new Drink.Cola();
+    //System.out.println(myDrink.Name);
+    //myDrink.display();
+    /*Menu myMenu = new Menu();
+    Order myOrder = new Order();
+    Receipt myReceipt = new Receipt();
+    
+    myOrder.Order();
+    myReceipt.displayReceipt();*/
+   
+    
+    
+    //myMenu.DrinkMenuDisplay();
+    //myMenu.FoodMenuDisplay();
+    //myMenu.DessertMenuDisplay();
+    
+    
+  }
+}
