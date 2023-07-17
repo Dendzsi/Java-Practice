975580a4e44da70ecf31ff4854c3ae6d048f69a1
Dendzsi <137180234+Dendzsi@users.noreply.github.com>
2023-07-17 11:45:21 +0200

    Create Drink.java
    Constructor update with super

---
+++ Drink.java
@@ -0,0 +1,14 @@
+
+class Drink {
+	protected String ID;
+    protected String Name;
+    protected double Price;
+    protected String Description;
+  
+  	public Drink(String ID, String Name, double Price, String Description){
+      this.ID = ID;
+      this.Name = Name;
+      this.Price = Price;
+      this.Description = Description;
+    }
+}
